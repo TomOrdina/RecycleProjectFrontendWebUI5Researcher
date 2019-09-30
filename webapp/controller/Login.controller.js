48b4bbeacc6a9095be4acf56f27a2625ea6a05ef
EbonyCyberpunk <kristof.salens@gmail.com>
2019-09-30 14:06:00 +0000

    Added Profile view + controller + adjusted Manifest for routing and target

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,11 +1,17 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/UIComponent"
+], function (Controller, UIComponent) {
 	"use strict";
 
 	return Controller.extend("ResearcherApp.RecycleFrontEndResearcherApp.controller.Login", {
 		onInit: function () {
 
+		},
+		
+		onLogin: function() {
+			var oRouter = UIComponent.getRouterFor(this);
+			oRouter.navTo("RouteProfile");
 		}
 	});
 });
