99d8343b218d53b7517ddaa3f10afcaf8b8724cc
Matthijs Huyghe <matthijs.huyghe@ordina.be>
2019-10-18 12:48:33 +0000

    Adding logic to item detail page and initialized routing

--- webapp/controller/DetailPage-Item.controller.js
+++ webapp/controller/DetailPage-Item.controller.js
@@ -3,47 +3,124 @@ sap.ui.define([
 	"sap/ui/core/routing/History"
 ], function (Controller, History) {
 	"use strict";
-
+	
 	return Controller.extend("ResearcherApp.RecycleFrontEndResearcherApp.controller.DetailPage-Item", {
-
+		
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf ResearcherApp.RecycleFrontEndResearcherApp.view.DetailPage-Item
 		 */
 		onInit: function () {
-			//Er wordt hier de data van het andere model in de tabel gezet om te kunnen laten zien in de view
-			var oModel3 = this.getOwnerComponent().getModel("modeltest");
-			var oTable4 = this.getView().byId("specificItem");
-			oTable4.setModel(oModel3);
+			// Bind a function to this view so this will be called upon every time we route to this view
+			this._oRouter = this.getOwnerComponent().getRouter();
+			this._oRouter.getRoute("RouteDetailItem").attachMatched(this.initialize, this);
+		},
+		
+		initialize: function() {
+			var that = this;
 			
-			//Er wordt hier de data van het andere model in de tabel gezet om te kunnen laten zien in de view
-			var oModel4 = this.getOwnerComponent().getModel("modeltest");
-			var oTable5 = this.getView().byId("specificItemDetail");
-			oTable5.setModel(oModel4);
+			// Get selected correlationAssetId from the model
+			var oItem = sap.ui.getCore().getModel("correlelationAssetId");
+			var correlationAssetId = JSON.parse(oItem.getJSON()).correlationAssetId.text;
+			
+			// Set weburl
+			var weburl = "https://eks.ordina-jworks.io/zpr-bff/assets/" + correlationAssetId;
+			
+			$.ajax({
+				url: weburl,
+				type: "GET",
+				dataType: "json",
+				success: function(dataj){
+					var activeJourneyLocations = [];
+					
+					// Set physicalID as title of the page
+					that.getView().byId("pageTitle").setTitle("ID: " + dataj.physicalId);
+					
+					dataj.assetJourneys.forEach(function(journey){
+						if (journey.isTravelling)
+						{
+							// Show entered time and date of the object 
+							if (journey.locations[0].timestamp) {
+								var timestamp = journey.locations[0].timestamp.split("T");
+								var date = timestamp[0].substring(0, timestamp[0].length - 1);
+								var time = timestamp[1].substring(0, timestamp[1].length - 1);
+								
+								that.getView().byId("labelTitle").setText("Entered the water on " + date + " at " + time);
+							}
+							
+							journey.locations.forEach(function(location) {
+								// Convert coordinates to dms
+								var dms = that.convertDMS(location.latitude, location.longitude);
+								
+								var timestamp = journey.locations[0].timestamp.split("T");
+								var aDate = timestamp[0].substring(0, timestamp[0].length - 1).split("-");
+								var aTime = timestamp[1].substring(0, timestamp[1].length - 1).split(":");
+								var convertedTimestamp = (aTime[1] + ":" + aTime[2] + " " + aDate[2] + "/" + aDate[1]);
+								
+								activeJourneyLocations.push({ "dms" : dms, "timestamp" : convertedTimestamp});
+							});
+						}
+					});
+					
+					// Create JSON model and set data
+					var oModel = new sap.ui.model.json.JSONModel();
+					oModel.setData(activeJourneyLocations);
+					
+					// Set new model at table from the view
+					var oTable = that.getView().byId("itemTable");
+					oTable.setModel(oModel);
+				},
+				error: function(){
+					// Error handling
+				}
+			});
 		},
 		
 		onNavBack: function () {
 			var oHistory = History.getInstance();
 			var sPreviousHash = oHistory.getPreviousHash();
-
+			
 			if (sPreviousHash !== undefined) {
 				window.history.go(-1);
 			} else {
 				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 				oRouter.navTo("RouteOverview", true);
 			}
+			
+			//location.reload();
+		},
+		
+		convertDMS: function (lat, lng) {
+			var latitude = this.toDegreesMinutesAndSeconds(lat);
+			var latitudeCardinal = lat >= 0 ? "N" : "S";
+			
+			var longitude = this.toDegreesMinutesAndSeconds(lng);
+			var longitudeCardinal = lng >= 0 ? "E" : "W";
+			
+			return latitude + latitudeCardinal + " " + longitude + longitudeCardinal;
+		},
+		
+		toDegreesMinutesAndSeconds: function (coordinate) {
+			var absolute = Math.abs(coordinate);
+			var degrees = Math.floor(absolute);
+			var minutesNotTruncated = (absolute - degrees) * 60;
+			var minutes = Math.floor(minutesNotTruncated);
+			var seconds = Math.floor((minutesNotTruncated - minutes) * 60);
+			
+			return degrees + "°" + minutes + "'" + seconds + '"' ;
 		}
-
+		
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
 		 * @memberOf ResearcherApp.RecycleFrontEndResearcherApp.view.DetailPage-Item
 		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
+		// onBeforeRendering: function() {
+		// 	this.initialize();
+		// }
+	
+	
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
@@ -52,7 +129,7 @@ sap.ui.define([
 		//	onAfterRendering: function() {
 		//
 		//	},
-
+		
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
 		 * @memberOf ResearcherApp.RecycleFrontEndResearcherApp.view.DetailPage-Item
@@ -60,7 +137,5 @@ sap.ui.define([
 		//	onExit: function() {
 		//
 		//	}
-
 	});
-
 });
--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -24,20 +24,20 @@ sap.ui.define([
 				
 				// Enable 'previous' pagination button
 				that.getView().byId("previousButton").setEnabled(true);
-				
+					
 				// Set pagination info
 				if ((clicks + 1) >= assetsSlicedLength) {
 					that.getView().byId("paginationInfo").setText(((clicks * 100) + 1) + "-" + assets.length+ " / " + assets.length);
 				}
 				else {
 					that.getView().byId("paginationInfo").setText(((clicks * 100) + 1) + "-" + ((clicks + 1) * 100) + " / " + assets.length);
-					
-					// Increment clicks as the 'next' pagination button has been clicked on
-					clicks += 1;
 				}
+				
+				// Increment clicks as the 'next' pagination button has been clicked on
+				clicks += 1;
 			}
 			
-			if (clicks >= assetsSlicedLength) {
+			if (clicks >= (assetsSlicedLength)) {
 				// Disable 'next' pagination  button
 				oEvt.getSource().setEnabled(false);
 			}
@@ -119,39 +119,6 @@ sap.ui.define([
 				type: "GET",
 				dataType: "json",
 				success: function(dataj){
-					// oModel.setData({ "item": {"correlationAssetId" : dataj.id }}, true);
-					// // Navigates to the succes page
-					// oRouter.navTo("Succes");
-					
-					// var oModel2 = new sap.ui.model.json.JSONModel();
-					// oModel2.loadData("model/data2.json")
-					// console.log(oModel2);
-					
-					// console.log("stringify ajax object:");
-					// var test = JSON.stringify(dataj);
-					// console.log(test);
-					
-					// console.log("parse stringified ajax object:");
-					// var test1 = JSON.parse(test);
-					// console.log(test1);
-					
-					// console.log("add new word to stringified ajax object");
-					// var test4 = '{ "Item" :' + test + '}';
-					// console.log(test4);
-					
-					// console.log("parse stringified ajax object");
-					// var test5 = JSON.parse(test4);
-					// console.log(test5);
-					
-					// console.log("extra test: ");
-					// var test6 = JSON.parse('{	"Item": [{		"correlationAssetId": "ded5833c-947c-48a3-920f-88a8c6743a29",		"active": true,		"type": {			"Type_name": "sensor",			"ColorCode": "new",			"batteryLevel": 15,			"wakeUpTime": 15000,			"hibernate": false		},		"status": "UNCONNECTED",		"locations": [{			"id": "ef979d1c-3547-442f-aafc-daf04d5a5b47",			"latitude": 1.23,			"longitude": 4.57,			"timestamp": "2019-10-11T01:59:00Z"		}],		"FirstWaterEntered": "2019-02-15T10:30:00Z"	}]}');
-					// console.log(test6);
-					// //'{ "Item" :[{"id":"81053c82-449a-4627-9c10-d13697d502cf","physicalId":"R-001-II","active":null,"type":"UNCONNECTED"},{"id":"72941f30-fcd0-4fb7-8582-703c8f66d487","physicalId":"R-001-III","active":null,"type":"UNCONNECTED"},{"id":"a57c8c1b-2de9-4d9b-9893-108615022293","physicalId":"R-002-I","active":null,"type":"UNCONNECTED"},{"id":"8ad4a1b0-e952-408d-80c9-488f9224aa87","physicalId":"R-002-II","active":null,"type":"UNCONNECTED"},{"id":"6a768ee3-bd9a-4d87-a4af-5801ff5cd9f8","physicalId":"R-002-III","active":null,"type":"UNCONNECTED"},{"id":"06110b28-76e1-4a41-adfb-6ba0085ef064","physicalId":"R-003-I","active":null,"type":"UNCONNECTED"},{"id":"a444f1b2-7b17-47ea-bab0-f8e1e4f3e39f","physicalId":"R-003-II","active":null,"type":"UNCONNECTED"},{"id":"0269f806-a731-423e-86ef-12a1babf1125","physicalId":"R-003-III","active":null,"type":"UNCONNECTED"}'));
-					
-					// console.log("stringify local json object");
-					// var test2 = JSON.stringify("model/data2.json");
-					// console.log(test2);
-					
 					// Declare global variables
 					assets = dataj;
 					assetsSlicedLength = Math.ceil(dataj.length / 100);
@@ -190,235 +157,20 @@ sap.ui.define([
 		},
 		
 		onItemPress: function (event) {
-			// var oModel = event.getParameters().listItem;
-			//sap.ui.core().setMo	
-				
+			
+			var correlationAssetId = event.getParameters().listItem.mAggregations.cells[0].mProperties;
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.setData({'correlationAssetId' : correlationAssetId}, true);
+			
+			sap.ui.getCore().setModel(oModel, "correlelationAssetId");
+			
 			// Navigate to detail page
 			var oRouter = UIComponent.getRouterFor(this);
 			oRouter.navTo("RouteDetailItem");
-			
-			// //Dit is de data gehaald uit een individueel item waar is op geklikt
-			// //Deze wordt dan geplaatst in een ander model om vanuit dit model op een andere pagina de data terug uit te halen
-			// var correlationAssetIdProperty = event.getParameters().listItem.mAggregations.cells[1].mProperties;
-			// var correlationAssetId = correlationAssetIdProperty.text;
-			
-			// var activeproperty = event.getParameters().listItem.mAggregations.cells[3].mProperties;
-			// var active = activeproperty.text;
-			
-			// var statusproperty = event.getParameters().listItem.mAggregations.cells[2].mProperties;
-			// var status = statusproperty.text;
-			
-			// var typeNameProperty = event.getParameters().listItem.mAggregations.cells[4].mProperties;
-			// var typeName = typeNameProperty.text;
-			
-			// var FistWaterEnteredproperty = event.getParameters().listItem.mAggregations.cells[5].mProperties;
-			// var FistWaterEntered = FistWaterEnteredproperty.text;
-			
-			// var physicalIdproperty = event.getParameters().listItem.mAggregations.cells[11].mProperties;
-			// var physicalId = physicalIdproperty.text;
-			
-			// var latitudeproperty = event.getParameters().listItem.mAggregations.cells[7].mProperties;
-			// var latitude = latitudeproperty.text;
-			
-			// var longitudeproperty = event.getParameters().listItem.mAggregations.cells[6].mProperties;
-			// var longitude = longitudeproperty.text;
-			
-			// var timestampproperty = event.getParameters().listItem.mAggregations.cells[0].mProperties;
-			// var timestamp = timestampproperty.text;
-			// //Hierbij wordt de datum geherformatteerd zodat deze op de manier van de wireframes kan worden afgebeeld in de detail page
-			// //var year = timestamp.substring(0,4);
-			// var month = timestamp.substring(7,5);
-			// var day = timestamp.substring(10,8);
-			// var hour = timestamp.substring(13,11);
-			// var minutes = timestamp.substring(16,14);
-			// var time = hour+":"+minutes+"h "+day+"/"+month;
-			// //Hierbij wordt er gechecked of het item een sensor is, indien dit zo is gaat hij naar de sensor functie anders doet hij de else code
-			// if(typeName == "sensor"){
-			// 	this.sensor(event,latitude,longitude,time);
-			// }else{
-			// var oModel2 = this.getView().getModel("modeltest");
-			// //Hierbij steekt hij de bovenstaande data van het individueel item in een ander model
-			// oModel2.setData({
-			// 	"Item": [{
-			// 		"correlationAssetId": correlationAssetId,
-			// 		"active": active,
-			// 		"type": {
-			// 			"Type_name": typeName,
-			// 			"ColorCode": "",
-			// 			"physicalId": physicalId,
-			// 			"batteryLevel": "",
-			// 			"wakeUpTime": "",
-			// 			"hibernate": ""
-			// 		},
-			// 		"status": status,
-			// 		"locations": [{
-			// 			"id": "",
-			// 			"latitude": latitude,
-			// 			"longitude": longitude,
-			// 			"timestamp": time
-			// 		}],
-			// 		"FirstWaterEntered": FistWaterEntered
-			// 	}],
-			// 	"User": [{
-			// 		"UID": "",
-			// 		"Gebruikersnaam": "",
-			// 		"Wachtwoord": "",
-			// 		"Achternaam": "",
-			// 		"Voornaam": "",
-			// 		"Export": "",
-			// 		"Role": ""
-			// 	}]
-			// }, true);
-			// sap.ui.getCore().setModel(oModel2);
-			// }
 		},
 		
-		// sensor: function (event,latitude,longitude,timestamp) {
-		// 	//sensor
-		// 	//Dit is de data gehaald uit een individueel item waar is op geklikt
-		// 	//Deze wordt dan geplaatst in een ander model om vanuit dit model op een andere pagina de data terug uit te halen
-		// 	var batteryLevelproperty = event.getParameters().listItem.mAggregations.cells[8].mProperties;
-		// 	var batteryLevel = batteryLevelproperty.text;
-		// 	var wakeupTimeproperty = event.getParameters().listItem.mAggregations.cells[9].mProperties;
-		// 	var wakeupTime = wakeupTimeproperty.text;
-		// 	var hibernateproperty = event.getParameters().listItem.mAggregations.cells[10].mProperties;
-		// 	var hibernate = hibernateproperty.text;
-		// 	var oModel2 = this.getView().getModel("modeltest");
-		// 	//Hierbij steekt hij de bovenstaande data van het individueel item in een ander model
-		// 	oModel2.setData({
-		// 		"Item": [{
-		// 			"correlationAssetId": "",
-		// 			"active": "",
-		// 			"type": {
-		// 				"Type_name": "",
-		// 				"ColorCode": "",
-		// 				"batteryLevel": batteryLevel,
-		// 				"wakeUpTime": wakeupTime,
-		// 				"hibernate": hibernate
-		// 			},
-		// 			"status": "",
-		// 			"locations": [{
-		// 				"id": "",
-		// 				"latitude": latitude,
-		// 				"longitude": longitude,
-		// 				"timestamp": timestamp
-		// 			}],
-		// 			"FirstWaterEntered": ""
-		// 		}],
-		// 		"User": [{
-		// 			"UID": "",
-		// 			"Gebruikersnaam": "",
-		// 			"Wachtwoord": "",
-		// 			"Achternaam": "",
-		// 			"Voornaam": "",
-		// 			"Export": "",
-		// 			"Role": ""
-		// 		}]
-		// 	}, true);
-		// 	sap.ui.getCore().setModel(oModel2);
-			
-		// 	var oRouter = UIComponent.getRouterFor(this);
-		// 	oRouter.navTo("RouteDetailSensor");
-		// },
-			
-		// userEventPress: function (event) {
-		// 	//sensor
-
-		// 	var UIDproperty = event.getParameters().listItem.mAggregations.cells[0].mProperties;
-		// 	var UID = UIDproperty.text;
-		// 	console.log(UID);
-		// 	var Gebruikersnaamproperty = event.getParameters().listItem.mAggregations.cells[1].mProperties;
-		// 	var Gebruikersnaam = Gebruikersnaamproperty.text;
-		// 	console.log(Gebruikersnaam);
-		// 	var Wachtwoordproperty = event.getParameters().listItem.mAggregations.cells[2].mProperties;
-		// 	var Wachtwoord = Wachtwoordproperty.text;
-		// 	console.log(Wachtwoord);
-		// 	var Achternaamproperty = event.getParameters().listItem.mAggregations.cells[3].mProperties;
-		// 	var Achternaam = Achternaamproperty.text;
-		// 	console.log(Achternaam);
-		// 	var Voornaamproperty = event.getParameters().listItem.mAggregations.cells[4].mProperties;
-		// 	var Voornaam = Voornaamproperty.text;
-		// 	console.log(Voornaam);
-		// 	var Exportproperty = event.getParameters().listItem.mAggregations.cells[5].mProperties;
-		// 	var Export = Exportproperty.text;
-		// 	console.log(Export);
-		// 	var Roleproperty = event.getParameters().listItem.mAggregations.cells[6].mProperties;
-		// 	var Role = Roleproperty.text;
-		// 	console.log(Role);
-
-		// 	//get model from data.json file
-			
-		// 	var oModel2 = this.getView().getModel("modeltest");
-		// 	// Assign the model object to the SAPUI5 core
-		// 	oModel2.setData({
-		// 		"Item": [{
-		// 			"correlationAssetId": "",
-		// 			"active": "",
-		// 			"type": {
-		// 				"Type_name": "",
-		// 				"ColorCode": "",
-		// 				"batteryLevel": "",
-		// 				"wakeUpTime": "",
-		// 				"hibernate": ""
-		// 			},
-		// 			"status": "",
-		// 			"locations": [{
-		// 				"id": "",
-		// 				"latitude": "",
-		// 				"longitude": "",
-		// 				"timestamp": ""
-		// 			}],
-		// 			"FirstWaterEntered": ""
-		// 		}],
-		// 		"User": [{
-		// 			"UID": UID,
-		// 			"Gebruikersnaam": Gebruikersnaam,
-		// 			"Wachtwoord": Wachtwoord,
-		// 			"Achternaam": Achternaam,
-		// 			"Voornaam": Voornaam,
-		// 			"Export": Export,
-		// 			"Role": Role
-		// 		}]
-		// 	}, true);
-			
-		// 	sap.ui.getCore().setModel(oModel2);
-		// 	var oRouter = UIComponent.getRouterFor(this);
-		// 	oRouter.navTo("RouteSecond");
-		// },
 		onAfterRendering: function () {
-			// var that = this;
 			
-			//get model from data.json file
-			
-			
-			//var oModel = that.getView().getModel("data");
-			
-			
-			//var localModel = JSON.parse(oModel.getJSON());
-			//console.log(localModel);
-			
-			
-			//that.getView().setModel(oModel);
-			
-			
-			//var loModel = JSON.parse(oModel);
-			//console.log(loModel);
-//<<<<<<< HEAD
-			// var datum = "teststring";
-			// var text = new sap.m.Text("test");
-			// text.setText(datum);
-//=======
-			//var datum = "teststring";
-			//var text = new sap.m.Text("test");
-			//text.setText(datum);
-			
-			// var test = sap.ui.getCore().getElementById("scrollBarOverview");
-			// var test2 = that.getView().byId("scrollBarOverview").getDomRef();
-			// var test3 = that.getView().byId("scrollBarOverview"); //.getDomRef();
-			
-			// test2.classList.add("noScrollBarWebkit");
-			// test2.className += "noScrollBarWebkit";
-//>>>>>>> refs/heads/applic
 		}
 	});
 });
